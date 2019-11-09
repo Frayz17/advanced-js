6dfea3f1ed5b6871afb4118dfd31751d0ddf575d
spalah <test@test.com>
2019-11-09 17:52:31 +0200

    create base slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import DynamicSearch from './modules/DynamicSearch';
+import Slider from './modules/Slider';
+import Block from './components/Block';
 
 class App extends React.Component {
 	render = () => {
 		return <>
-			<DynamicSearch />
+			<Slider>
+				<Block style={{ backgroundColor: 'red' }}>
+					First slide
+				</Block>
+				<Block style={{ backgroundColor: 'blue' }}>
+					Second slide
+				</Block>
+				<Block style={{ backgroundColor: 'grey' }}>
+					Third slide
+				</Block>
+				<Block style={{ backgroundColor: 'yellow' }}>
+					Forth slide
+				</Block>
+			</Slider>
 		</>;
 	};
 };
