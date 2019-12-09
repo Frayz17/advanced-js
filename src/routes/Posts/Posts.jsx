cd3d6fb2dae25e00c9c74169a8c9cce790109838
Andrey Dmitriev <andrydmit@gmail.com>
2019-12-09 23:33:24 +0200

    divide posts componen t into several components.

--- src/routes/Posts/Posts.jsx
+++ src/routes/Posts/Posts.jsx
@@ -1,45 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Post from 'modules/Post';
-import { TypographyHeader } from 'components/Typography';
-import loadPosts from './loadPosts.js';
-import infinityLoad from './infinityLoad';
-import resetPosts from './resetPosts';
+import PostsHeader from './PostsHeader.jsx';
+import PostsList from './PostsList.jsx';
 
-export default connect((state) => {
-  return {
-    postsLength: (state.posts.data || []).length || 0
-  };
-})(
-  React.memo(({ postsLength }) => {
-    // onMount
-    React.useEffect(() => {
-      const localInfinityLoad = infinityLoad;
-
-      loadPosts(1);
-      window.addEventListener('scroll', localInfinityLoad());
-
-      // onUnmount
-      return () => {
-        resetPosts();
-        window.removeEventListener('scroll', localInfinityLoad());
-      };
-    }, []);
-
-    return (
-      <>
-        <TypographyHeader>Posts page</TypographyHeader>
-        {(() => {
-          let i = 0,
-            collector = [];
-
-          while (i < postsLength) {
-            collector.push(<Post key={i} index={i} />);
-            i++;
-          }
-          return collector;
-        })()}
-      </>
-    );
-  })
-);
+export default React.memo(({ postsLength }) => {
+  return (
+    <>
+      <PostsHeader />
+      <PostsList />
+    </>
+  );
+});
--- src/routes/Posts/loadPosts.js
+++ src/routes/Posts/loadPosts.js
@@ -1,21 +1,10 @@
 import { getStore } from 'Services/Store';
 
-const enableFetch = (timer) => {
-  let cooldown = null;
-
-  clearTimeout(cooldown);
-  cooldown = setTimeout(() => {
-    getStore().dispatch({
-      type: 'ENABLE_TO_FETCH_DATA_AFTER_FAIL'
-    });
-  }, timer);
-};
-
 export default async (page = 1) => {
   try {
     const response = await fetch(`http://127.0.0.1:4000/posts/${page}`);
 
-    if (response.ok) {
+    if (response.status === 200) {
       const data = await response.json();
 
       getStore().dispatch({
@@ -29,18 +18,10 @@ export default async (page = 1) => {
       getStore().dispatch({
         type: 'FETCH_POSTS_DATA_FAILED'
       });
-      console.log('faild to get posts, response status: ', response.status);
-
-      // enable to fetch again after 10 seconds
-      enableFetch(10000);
     }
-  } catch (e) {
-    console.log(e);
+  } catch {
     getStore().dispatch({
       type: 'FETCH_POSTS_DATA_FAILED'
     });
-
-    // enable to fetch again after 10 seconds
-    enableFetch(10000);
   }
 };
