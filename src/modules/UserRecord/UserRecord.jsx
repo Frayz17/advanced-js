0c50272710990ba73e375aabfc46e6abdc4e2fe4
Andrey <andrydmit@gmail.com>
2019-12-23 17:45:40 +0200

    removed connect from UserRecord module

--- src/modules/UserRecord/UserRecord.jsx
+++ src/modules/UserRecord/UserRecord.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { getState } from 'Services/Store';
 import { BlockFlex } from 'components/Block';
-import Typography from 'components/Typography';
+import Typography, { TypographyHeader } from 'components/Typography';
 
-export default connect((state, { stateName, index = 0 }) => {
-  // console.log(state[stateName]);
-  const { data } = state[stateName];
-  return { ...data[index] };
-})(
-  React.memo((data) => {
-    return (
+export default React.memo(({ stateName, index }) => {
+  const { data } = getState()[stateName];
+  const userData = data[index];
+
+  return (
+    <BlockFlex
+      style={{
+        minHeight: '300px',
+        width: '300px',
+        backgroundColor: 'lightblue',
+        margin: '10px 0',
+        padding: '15px',
+        border: '1px solid grey'
+      }}
+    >
       <BlockFlex
         style={{
-          minHeight: '300px',
-          width: '300px',
-          backgroundColor: 'lightblue',
-          margin: '10px 0',
-          padding: '15px',
-          border: '1px solid grey'
+          flexDirection: 'column',
+          justifyContent: 'space-around',
+          alignItems: 'center'
         }}
       >
-        <BlockFlex
-          style={{
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center'
-          }}
-        >
-          <Typography>{data.name}</Typography>
-          <Typography>{data.text}</Typography>
-          <Typography>{data.created_at}</Typography>
-        </BlockFlex>
+        <TypographyHeader>{userData.name}</TypographyHeader>
+        <Typography>{userData.text}</Typography>
+        <Typography>{userData.created_at}</Typography>
       </BlockFlex>
-    );
-  })
-);
+    </BlockFlex>
+  );
+});
--- src/routes/Posts/Posts.jsx
+++ src/routes/Posts/Posts.jsx
@@ -3,14 +3,12 @@ import Post from 'modules/Post';
 import LoaderIterables from 'modules/LoaderIterables';
 import { TypographyHeader } from 'components/Typography';
 
-const STATE_LOADER_DATA_NAME = 'loaderIterablesPosts';
-
 export default React.memo(() => {
   return (
     <>
       <TypographyHeader>Posts page</TypographyHeader>
       <LoaderIterables
-        stateName={STATE_LOADER_DATA_NAME}
+        stateName={'loaderIterablesPosts'}
         url="http://127.0.0.1:4000"
         query="/posts/"
         urlId={1}
